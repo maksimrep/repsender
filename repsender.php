75f01bb8cfaa03517d5851b400aec1139555bead
Maksim Rep <mr@signum.ai>
2022-09-19 22:45:28 +0300

    init plugin

---
+++ repsender.php
@@ -0,0 +1,14 @@
+<?php
+/*
+ * Plugin Name: Rep Sender
+ * Description: WordPress plugin for group mailing
+ * Author: Maksym Repetskyi
+ * Version: 0.0.1
+ * Text Domain:  repsender
+ * Domain Path:  /languages/
+ * Requires PHP: 7.4
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Don't access directly.
+};
